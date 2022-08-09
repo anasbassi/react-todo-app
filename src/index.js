771a3999ef2412ed08f8d8a2a4384d5e5865136d
Anas Abubakar <anasabubakar2015@gmail.com>
2022-08-09 13:26:04 +0100

    Add the navbar for the todos

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 import React from "react"
-import ReactDOM from "react-dom"
-import TodoContainer from "./components/TodoContainer"
-import "./App.css"
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import TodoContainer from "./functionBased/components/TodoContainer"
+import Navbar from "./functionBased/components/Navbar"
+import About from "./pages/About"
+import NotMatch from "./pages/NotMatch"
+import "./functionBased/App.css"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <TodoContainer />
-  </React.StrictMode>,
-  document.getElementById("root")
-)
+const App = () => {
+  return (
+    <React.StrictMode>
+      <BrowserRouter>
+        <Routes>
+          <Route path="" element={<Navbar />}>
+            <Route index element={<TodoContainer />} />
+            <Route path="about" element={<About />} />
+            <Route path="*" element={<NotMatch />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </React.StrictMode>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
---
+++ src/pages/About.js
@@ -0,0 +1,6 @@
+import React from "react"
+
+const About = () => {
+  return <div className="about__content">hello from about page</div>
+}
+export default About
